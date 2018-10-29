dbe88426e27ad1686054af5f1874d7bdf2952d58
Jovanela Daribo <carly972@live.fr>
2018-10-29 14:11:55 +0100

    Quête Symfony 3

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jovanela
+ * Date: 29/10/18
+ * Time: 13:36
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route ("/", name="Hello")
+     */
+    public function index()
+    {
+        return $this->render('Home.html.twig');
+    }
+}
---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,24 @@
+<?php
+// src/Controller/LuckyController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    public function number()
+    {
+        /**
+         * @Route("/lucky/number")
+         *
+        */
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
