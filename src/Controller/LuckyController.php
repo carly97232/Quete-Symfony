3c1005865fa28f0598b429b036d8d4f2ac7a715d
Jovanela Daribo <carly972@live.fr>
2018-10-29 15:14:32 +0100

    Ajout de lucky

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -9,16 +9,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController extends AbstractController
 {
+    /**
+     * @Route("/lucky/number")
+     */
     public function number()
     {
-        /**
-         * @Route("/lucky/number")
-         *
-        */
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
