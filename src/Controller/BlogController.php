be78f30ef24d972d1b9c4a4dfa0c60dd55f0b27c
Jovanela Daribo <carly972@live.fr>
2018-11-08 11:22:27 +0100

    Quête Symfony4 Routing avancé.

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jovanela
+ * Date: 04/11/18
+ * Time: 20:51
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog/{page}", requirements={"page"="\d+"}, name="blog_list")
+     * @param $page
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function list($page)
+    {
+        return $this->render('blog/index.html.twig', ['page' => $page]);
+    }
+    /**
+     * @Route("/blog/{slug}", requirements={"slug"="[a-z{0-9}-]+"}, name="blog_show")
+     * @param $slug
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show($slug='Article Sans Titre')
+    {
+        $slug = ucwords(str_replace("-", " ", $slug));
+        return $this->render('blog/article.html.twig', ['slug' => $slug]);
+    }
+}
