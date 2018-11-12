2404c1e60e86f616b064c73ee8c75b0d7774e26e
Jovanela Daribo <carly972@live.fr>
2018-11-12 12:50:55 +0100

    Quete7 de Symfony4.

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,7 +8,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
@@ -22,14 +24,38 @@ class BlogController extends AbstractController
     {
         return $this->render('blog/index.html.twig', ['page' => $page]);
     }
+
     /**
      * @Route("/blog/{slug}", requirements={"slug"="[a-z{0-9}-]+"}, name="blog_show")
      * @param $slug
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function show($slug='Article Sans Titre')
+    public function show($slug = 'Article Sans Titre')
     {
         $slug = ucwords(str_replace("-", " ", $slug));
         return $this->render('blog/article.html.twig', ['slug' => $slug]);
+    }
+
+    /**
+     * Show all row from article's entity
+     *
+     * @Route("/", name="blog_index")
+     * @return Response A response instance
+     */
+    public function index() : Response
+    {
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table.'
+            );
+        }
+        return $this->render(
+            'blog/index.html.twig',
+            ['articles' => $articles]
+        );
     }
 }
