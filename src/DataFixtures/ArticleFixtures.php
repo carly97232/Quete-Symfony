5bf40391dbced96141c14bcc48e10f01bf52dad1
Jovanela Daribo <carly972@live.fr>
2018-12-06 13:29:51 +0100

    Quête fixtures.

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jovanela
+ * Date: 04/12/18
+ * Time: 21:03
+ */
+
+namespace App\DataFixtures;
+
+use Faker;
+use App\Entity\Category;
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ArticleFixtures extends Fixture implements DependentFixtureInterface
+{
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+        $categories = $manager->getRepository(Category::class)->findBy([], [], 5);
+
+        foreach ($categories as $category) {
+            for ($i = 1; $i <= 10; $i++) {
+                $article = new Article();
+                $content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
+                $article->setTitle(mb_strtolower($faker->sentence()));
+                $article->setContent($content);
+                $manager->persist($article);
+                $article->setCategory($category);
+                $manager->flush();
+            }
+        }
+    }
+
+    public function getDependencies()
+    {
+        return [CategoryFixtures::class];
+    }
+}
